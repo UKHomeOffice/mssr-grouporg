abcc90a5db4cc153404f50b2c43015d2a023a681
rjaini-HO <ramana.jaini@digital.homeoffice.gov.uk>
2022-07-02 17:31:10 +0100

    changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Modern Slavery Statement Registry',
+  serviceName: 'Modern slavery statement registry',
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -27,13 +27,13 @@ router.post('/3_FY-1', function (req, res)
    })
 
 // PDF document delete check yes and no
-router.post('/6S1-3-FY-PDF/S13-4-DeleteConfirm', function (req, res)
+router.post('/new-user/6S1-3-FY-PDF/S13-4-DeleteConfirm', function (req, res)
   {
     const selectedOption = req.body['delete-document']
      let error = {}
      if (!selectedOption) {
        error = { text: "Select an option" }
-      return res.render("6S1-3-FY-PDF/S13-4-DeleteConfirm", { error })}
+      return res.render("/new-user/6S1-3-FY-PDF/S13-4-DeleteConfirm", { error })}
 
      // Make a variable and give it the value from 'fy-year'
      var deletedoc = req.session.data['delete-document']
@@ -41,23 +41,23 @@ router.post('/6S1-3-FY-PDF/S13-4-DeleteConfirm', function (req, res)
      // Check to confirm the deletion
      if (deletedoc == "yes"){
        // Send user to next page
-       res.redirect('/6S1-3-FY-PDF/S13-5-Deleted')
+       res.redirect('/new-user/6S1-3-FY-PDF/S13-5-Deleted')
      }
      else {
        // Cancelling the deletion
-       res.redirect('/6S1-3-FY-PDF/S13-5B-Deleted')
+       res.redirect('/new-user/6S1-3-FY-PDF/S13-3A-PDFUploaded')
      }
    })
 
 
 // Scoping check yes and no
-router.post('/6S1-2-Scoping/S12-1-ScopingQ', function (req, res)
+router.post('/new-user/6S1-2-Scoping/S12-1-ScopingQ', function (req, res)
 {
   const selectedOption = req.body['scoping']
   let error = {}
   if (!selectedOption) {
     error = { text: "Select an option" }
-    return res.render("6S1-2-Scoping/S12-1-ScopingQ", { error })}
+    return res.render("/new-user/6S1-2-Scoping/S12-1-ScopingQ", { error })}
 
   // Make a variable and give it the value from 'scopingconfirm'
   var scopingconfirm = req.session.data['scoping']
@@ -65,11 +65,11 @@ router.post('/6S1-2-Scoping/S12-1-ScopingQ', function (req, res)
   // YES, company has to subnit the statement
   if (scopingconfirm == "yes"){
     // Send user to next page
-    res.redirect('/6S1-2-Scoping/S12-3-YesConfirm')
+    res.redirect('/new-user/6S1-2-Scoping/S12-3-YesConfirm')
   }
   else {
     // NO, company do not have to subnit the statement
-    res.redirect('/6S1-2-Scoping/S12-2-NoSelection')
+    res.redirect('/new-user/6S1-2-Scoping/S12-2-NoSelection')
   }
 })
 
